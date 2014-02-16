b7b406732fd13959dd8ce47f524bd099469fca05
Geoffrey Dagley <gdagley@gmail.com>
2014-02-16 14:05:09 -0600

    rename active model lint test so it runs.

--- test/test_active_model_lint.rb
+++ test/test_active_model_lint.rb
@@ -3,10 +3,11 @@ require 'parse_resource'
 
 ParseResource::Base.load!(ENV["PARSE_RESOURCE_APPLICATION_ID"], ENV["PARSE_RESOURCE_MASTER_KEY"])
 
-class Bowl < ParseResource
+class Bowl < ParseResource::Base
   fields :title, :body, :author
   validates_presence_of :title
 end
+
 class ActiveModelLintTest < ActiveModel::TestCase
   include ActiveModel::Lint::Tests
 
